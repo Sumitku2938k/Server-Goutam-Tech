59ee6780050858171b82b94d76179a1185b435a8
Sumit Kumar <sumitku2938k@gmail.com>
2026-02-07 16:30:17 +0530

    Securing our Admin Route with JWT Verification and Middleware Integration

--- router/admin-router.js
+++ router/admin-router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { getAllUsers, getAllContacts } = require("../controllers/admin-controller");
+const authMiddleware = require("../middlewares/auth-middleware");
+
+// Apply auth middleware to all routes in this router
+router.use(authMiddleware);
 
 router.route("/users").get(getAllUsers);
 router.route("/contacts").get(getAllContacts);
